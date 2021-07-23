5726183d43bc0f87a39f0a96ff0719d702c4cff8
thaistardy <thaistardy.s@gmail.com>
2021-07-23 09:25:04 -0300

    Removendo function não utilizada

--- assets/script.js
+++ assets/script.js
@@ -12,15 +12,6 @@ document.querySelector('.composer button').addEventListener( 'click', ()=> {
   }
 })
 
-function randomColors() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += `${letters[Math.floor(Math.random() * 16)]}`;
-  }
-  return color;
-}
-
 function playSound(sound) {
   let audioElement = document.querySelector(`#s_${sound}`);
   let keyElement = document.querySelector(`div[data-key="${sound}`);
